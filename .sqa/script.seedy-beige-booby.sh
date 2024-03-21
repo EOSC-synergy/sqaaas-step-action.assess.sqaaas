3701735e11123fb37c1d9c72e5f0f6d1f03d37a4
Pablo Orviz <orviz@ifca.unican.es>
2024-03-21 11:19:25 +0100

    Add JePL file structure

---
+++ .sqa/script.seedy-beige-booby.sh
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: Copyright contributors to the Software Quality Assurance as a Service (SQAaaS) project <sqaaas@ibergrid.eu>
+# SPDX-FileContributor: Pablo Orviz <orviz@ifca.unican.es>
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+(
+cd github.com/EOSC-synergy/sqaaas-step-action &&
+    make torch-cpu&&
+    make tf-2.13&&
+    micromamba run -p ./.venv-pytorch pip install .[dev]&&
+    micromamba run -p ./.venv-pytorch pytest -v ./tests/ -m "not slurm"
+)
